63f20d2fe0fd7a1fde3b062b6201457f59c4f219
shivam7374 <shivamsingla491@gmail.com>
2020-05-11 20:08:26 +0530

    Add

---
+++ Batch Code Generation/seeds.js
@@ -0,0 +1,23 @@
+const{Season, Batch, Course, Teacher, Center}=require('./model')
+const {db}=require('./connection')
+const seed= async()=>{
+    try{
+        db.sync({alter:true})
+        
+        await Center.bulkCreate([
+            {id:'PP' ,name:'Pitampura',city:'NewDelhi'},
+            {id:'DW' ,name:'Dwarka',city:'NewDelhi'},
+            {id:'NO' ,name:'Noida',city:'NewDelhi'},
+            {id:'DD' ,name:'Dehradun',city:'Dehradun'},
+            {id:'OL' ,name:'Online',city:'NewDelhi'},
+        ],{
+            ignoreDuplicates:true
+        })
+
+
+    }catch(e){
+        console.log(e)
+    }
+}
+
+seed();
