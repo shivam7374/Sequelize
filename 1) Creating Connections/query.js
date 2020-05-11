5c16c1bc55e22195f58a8e1f4248916f1f4e0853
shivam7374 <shivamsingla491@gmail.com>
2020-05-11 15:17:57 +0530

    Query

--- 1) Creating Connections/query.js
+++ 1) Creating Connections/query.js
@@ -3,7 +3,9 @@ const{db, Student}=require('./inserting entities')
 const task= async()=>{
 try{
     await db.sync()
-    const students=await Student.findAll()
+    const students=await Student.findAll({
+        where: {age: 14}
+    })
     students.forEach(s => {
         console.log(`
         name:${s.dataValues.name} \t\t age:${s.dataValues.age}
